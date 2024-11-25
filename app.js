8fd32ef97cc5a15c02780195d3b212e219a26ef1
Craig <craigjrobinson@hotmail.co.uk>
2024-11-25 14:12:12 +0000

    Completed topics challenge

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
-const { fetchObject } = require("./controller.js");
+const { fetchObject, fetchTopics } = require("./controller.js");
 
 app.get("/api", fetchObject);
+
+app.get("/api/topics", fetchTopics);
+
+app.use((req, res) => {
+  res.status(404).send("404 - Page Not Found");
+});
 
 module.exports = app;
